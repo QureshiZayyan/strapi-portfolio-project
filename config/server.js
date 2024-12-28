071dc8a242083c6a68a9a3506b2d68098f647728
Zayyan-TSA <zayyan.tsa@gmail.com>
2024-12-29 04:45:49 +0530

    fc

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+  port: env.int('PORT', 1337),
+  keys: [
+    env('APP_KEY_1'),
+    env('APP_KEY_2'),
+  ],
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
