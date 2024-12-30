b9c36e24b0b260d6861d443c9a12799e1162e507
Zayyan-TSA <zayyan.tsa@gmail.com>
2024-12-30 20:40:18 +0530

    push

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,9 @@
-module.exports = () => ({});
+// module.exports = () => ({});
+
+module.exports = ({ env }) => ({
+    "users-permissions": {
+        config: {
+            jwtSecret: '0sWeQWSvQh1+o53yzzD7DQ==', // Replace with the key you got from step 1
+        },
+    },
+});
