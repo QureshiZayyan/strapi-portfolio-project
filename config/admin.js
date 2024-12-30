bab0707373e3ada3b0cb17544f7139b1f7d0ae41
Zayyan-TSA <zayyan.tsa@gmail.com>
2024-12-30 20:48:07 +0530

    push

--- config/admin.js
+++ config/admin.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: '2VHfxfy/gK9IQIyYzLK4Yw==',
   },
   transfer: {
     token: {
