4d04036a383ffa49c7e987e4b5ec4277957241ed
Zayyan-TSA <zayyan.tsa@gmail.com>
2025-01-02 20:57:51 +0530

    push

--- config/database.js
+++ config/database.js
@@ -24,24 +24,23 @@ module.exports = ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        connectionString: env('DATABASE_URL'), // Use Render's DATABASE_URL environment variable directly
+        host: env('DATABASE_HOST', '127.0.0.1'), // You can leave this out if using DATABASE_URL
+        port: env.int('DATABASE_PORT', 5432), // Usually, you don't need to specify this if DATABASE_URL is set
+        database: env('DATABASE_NAME', 'strapi'), // DATABASE_URL will already include the database name
+        user: env('DATABASE_USERNAME', 'strapi'), // DATABASE_URL will already include the username
+        password: env('DATABASE_PASSWORD', 'strapi'), // DATABASE_URL will already include the password
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true), // This is to control whether the SSL certificate is verified or not
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: {
+        min: env.int('DATABASE_POOL_MIN', 2),
+        max: env.int('DATABASE_POOL_MAX', 10),
+      },
     },
+
     sqlite: {
       connection: {
         filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '/mnt/data/data.db')),
