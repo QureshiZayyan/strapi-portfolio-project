b94ff0b35ee679e8d849554d29463b3c135d461f
Zayyan-TSA <zayyan.tsa@gmail.com>
2025-01-03 21:48:07 +0530

    push

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
+  port: env.int('PORT', 1337),
+  url: env('RENDER_EXTERNAL_URL', 'https://strapi-portfolio-project-1.onrender.com'),
   app: {
     keys: [
       env('APP_KEY_1'),
